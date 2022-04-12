130481c65e2324ea1a6d9184eb6254c069491cca
MMaidul <maidulmuqtadir360@gmail.com>
2022-04-12 20:29:32 +0600

    Banner Component Done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Experts from '../Services/Experts/Experts';
 import Services from '../Services/Services';
 import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <>
+            <Banner />
             <Services />
             <Experts />
-        </div>
+        </>
     );
 };
 
