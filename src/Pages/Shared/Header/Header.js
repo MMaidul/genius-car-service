d39086585a7e9778ed0836a0e3822b235a4efbdf
MMaidul <maidulmuqtadir360@gmail.com>
2022-04-12 20:51:43 +0600

    header menu done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../../images/logo.png';
 
 const Header = () => {
     return (
-        <header>
-            <h2>This is header tag</h2>
+        <header className='sticky-top mb-1'>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={logo}
+                            width="200px"
+                            alt=""
+                        />
+                    </Navbar.Brand>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
         </header>
     );
 };
